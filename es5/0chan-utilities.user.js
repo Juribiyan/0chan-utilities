02eb3b589130c67d5585d6856e6e3de5af8c0a20
Juribiyan <snivy@ponyasha.ru>
2018-05-27 00:19:59 +0500

    Added support for 0chan.pl

--- es5/0chan-utilities.user.js
+++ es5/0chan-utilities.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         0chan Utilities
 // @namespace    http://0chan.hk/userjs
-// @version      2.1.0
+// @version      2.1.1
 // @description  Various 0chan utilities
 // @updateURL    https://github.com/Juribiyan/0chan-utilities/raw/Autohide/es5/0chan-utilities.meta.js
 // @author       Snivy [0xf330f91f]
 // @include      https://0chan.hk/*
 // @include      http://nullchan7msxi257.onion/*
 // @include      https://0chan.xyz/*
+// @include      https://0chan.pl/*
 // @grant        none
 // @icon         https://raw.githubusercontent.com/Juribiyan/0chan-utilities/master/icon.png
 // ==/UserScript==
