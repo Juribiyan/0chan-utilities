04d7768144680c5f7db51da71eac133d31023279
Juribiyan <snivy@ponyasha.ru>
2019-01-06 17:24:32 +0500

    Welcome ochan.ru

--- es5/0chan-utilities.meta.js
+++ es5/0chan-utilities.meta.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         0chan Utilities
 // @namespace    http://0chan.hk/userjs
-// @version      2.2.1
+// @version      2.2.2
 // @description  Various 0chan utilities
-// @updateURL    https://github.com/Juribiyan/0chan-utilities/raw/Autohide/es5/0chan-utilities.meta.js
+// @updateURL    https://github.com/Juribiyan/0chan-utilities/raw/master/es5/0chan-utilities.meta.js
 // @author       Snivy [0x88d27947]
 // @include      https://0chan.pl/*
 // @include      http://nullchpl673e6jo3.onion/*
 // @include      https://0chan.xyz/*
+// @include      https://ochan.ru/*
+// @include      http://ochan.ru/*
 // @grant        none
 // @icon         https://raw.githubusercontent.com/Juribiyan/0chan-utilities/master/icon.png
 // ==/UserScript==
