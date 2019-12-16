311ce03d92e7c7ebd6fe08095950eea9dffeaa7f
devarped <devar@redchan.it>
2019-12-16 20:41:42 +0300

    Claiming script, updating version and include lists

--- src/0chan-utilities.user.js
+++ src/0chan-utilities.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
 // @name         0chan Utilities
-// @namespace    http://0chan.hk/userjs
-// @version      2.2.5
+// @namespace    https://www.0chan.pl/userjs/
+// @version      2.2.6
 // @description  Various 0chan utilities
-// @updateURL    https://github.com/Juribiyan/0chan-utilities/raw/master/es5/0chan-utilities.meta.js
-// @author       Snivy [0x88d27947]
+// @updateURL    https://github.com/devarped/0chan-utilities/raw/master/es5/0chan-utilities.meta.js
+// @author       Snivy & devarped
+// @include      https://www.0chan.pl/*
 // @include      https://p.0chan.pl/*
-// @include      https://0chan.pl/*
+// @include      https://0.1chan.pl/*
+// @include      https://www.0chan.club/*
 // @include      http://nullchpl673e6jo3.onion/*
-// @include      https://0chan.xyz/*
 // @include      https://ochan.ru/*
-// @include      http://ochan.ru/*
 // @include      http://nullplctggmjazqcoboc2pw5anogckczzj6xo45ukrnsaxarpswu7sid.onion/*
 // @include      http://0pl.i2p/*
 // @include      http://gd7qe2pu2jwqabz4zcf3wwablrzym7p6qswczoapkm5oa5ouuaua.b32.i2p/*
 // @grant        none
-// @icon         https://raw.githubusercontent.com/Juribiyan/0chan-utilities/master/icon.png
+// @icon         https://raw.githubusercontent.com/devarped/0chan-utilities/master/icon.png
 // ==/UserScript==
 
 const icons =
